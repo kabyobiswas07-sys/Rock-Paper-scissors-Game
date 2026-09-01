909475174e855b2ed1c3d4de04e9df9b9a47af53
Kabyo Biswas <kabyobiswas07@gmail.com>
2026-09-01 11:26:05 +0600

    Week 6

--- src/model/DataModel.java
+++ src/model/DataModel.java
@@ -3,40 +3,43 @@ package model;
 
 public class DataModel {
 
-    
+   
     public static final String ROCK     = "Rock";
     public static final String PAPER    = "Paper";
     public static final String SCISSORS = "Scissors";
 
+    
+    public static final String WIN  = "WIN";     // ← NEW Week 6
+    public static final String LOSE = "LOSE";    // ← NEW Week 6
+    public static final String DRAW = "DRAW";    // ← NEW Week 6
+
+   
     private String playerChoice;
-    private String computerChoice;   // ← NEW this week
+    private String computerChoice;
+    private String result;                       
 
     public DataModel() {
         playerChoice   = "";
         computerChoice = "";
+        result         = "";
     }
 
    
-    public void setPlayerChoice(String choice) {
-        this.playerChoice = choice;
-    }
+    public void setPlayerChoice(String choice)   { this.playerChoice = choice; }
+    public String getPlayerChoice()              { return playerChoice; }
 
-    public String getPlayerChoice() {
-        return playerChoice;
-    }
-
-   
-    public void setComputerChoice(String choice) {          // ← NEW
-        this.computerChoice = choice;
-    }
-
-    public String getComputerChoice() {                     // ← NEW
-        return computerChoice;
-    }
+  
+    public void setComputerChoice(String choice) { this.computerChoice = choice; }
+    public String getComputerChoice()            { return computerChoice; }
 
    
+    public void setResult(String result)         { this.result = result; }   
+    public String getResult()                    { return result; }          
+
+  
     public void reset() {
         playerChoice   = "";
-        computerChoice = "";   // ← also cleared now
+        computerChoice = "";
+        result         = "";   
     }
 }
