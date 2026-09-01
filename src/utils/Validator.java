9cdb3e9b7e0e2baac34d6dd9a9af452ec5ae5fd4
Kabyo Biswas <kabyobiswas07@gmail.com>
2026-09-01 10:15:36 +0600

    Week 4

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -1,5 +1,22 @@
 package utils;
 
+import model.DataModel;
+
+
 public class Validator {
- 
+
+    
+    public static boolean isValidChoice(String input) {
+        return DataModel.ROCK.equals(input)
+            || DataModel.PAPER.equals(input)
+            || DataModel.SCISSORS.equals(input);
+    }
+
+    
+    public static String getErrorMessage(String input) {
+        if (input == null || input.isEmpty()) {
+            return "No choice made yet. Please click Rock, Paper, or Scissors.";
+        }
+        return "\"" + input + "\" is not a valid choice.";
+    }
 }
