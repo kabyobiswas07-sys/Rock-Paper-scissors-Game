d7325c53a0c01fa08efc417328d15094aec3b369
Kabyo Biswas <kabyobiswas07@gmail.com>
2026-09-01 10:12:02 +0600

    Week 4

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,40 @@
 package controller;
 
+import model.DataModel;
+import utils.Validator;
+
+
 public class Controller {
-    
+
+    private DataModel model;
+
+    public Controller() {
+        model = new DataModel();
+    }
+
+   
+    public String handlePlayerChoice(String choice) {
+
+       
+        if (!Validator.isValidChoice(choice)) {
+            return Validator.getErrorMessage(choice);
+        }
+
+        
+        model.setPlayerChoice(choice);
+
+        
+        return "You chose: " + model.getPlayerChoice()
+             + " — waiting for computer... (coming in Week 5)";
+    }
+
+    public String handleReset() {
+        model.reset();
+        return "Choose Rock, Paper, or Scissors!";
+    }
+
+   
+    public String getPlayerChoice() {
+        return model.getPlayerChoice();
+    }
 }
