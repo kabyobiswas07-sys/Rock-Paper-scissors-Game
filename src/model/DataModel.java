13ee852ad3d45b6ba8a0776bab6ebc61afd7fe8a
Kabyo Biswas <kabyobiswas07@gmail.com>
2026-09-01 10:14:08 +0600

    Week 4

--- src/model/DataModel.java
+++ src/model/DataModel.java
@@ -1,5 +1,32 @@
 package model;
 
+
 public class DataModel {
+
+    
+    public static final String ROCK     = "Rock";
+    public static final String PAPER    = "Paper";
+    public static final String SCISSORS = "Scissors";
+
    
+    private String playerChoice;
+
+    public DataModel() {
+        playerChoice = "";   
+    }
+
+   
+    public void setPlayerChoice(String choice) {
+        this.playerChoice = choice;
+    }
+
+    
+    public String getPlayerChoice() {
+        return playerChoice;
+    }
+
+    
+    public void reset() {
+        playerChoice = "";
+    }
 }
